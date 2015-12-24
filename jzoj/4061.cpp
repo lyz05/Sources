e1771cdafdf3743a41f399dcb01af0b364058617
liuyuanzhe <liuyuanzhe0515@163.com>
2015-12-24 17:06:58 +0800

    12.24

---
+++ jzoj/4061.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	freopen("4061.in","r",stdin);
+	//freopen(".out","w",stdout);
+
+	return 0;
+}
