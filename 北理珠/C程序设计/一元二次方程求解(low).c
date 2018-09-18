7cffaf40d031d0c9835906c5911aebd66d7569f7
lyz <liuyuanzhe0515@gmail.com>
2018-09-18 15:30:11 +0800

    UPDATE

--- 北理珠/C程序设计/一元二次方程求解(low).c
+++ 北理珠/C程序设计/一元二次方程求解(low).c
@@ -8,20 +8,13 @@ int main()
 	printf("a=");scanf("%d",&a);
 	printf("b=");scanf("%d",&b);
 	printf("c=");scanf("%d",&c);
-	//输入系数 
+	//scanf("%d%d%d",&a,&b,&c);
 	double delta = b*b-4*a*c,x1,x2;
 	x1 = (-b*1.0-sqrt(delta))/(2*a*1.0);
 	x2 = (-b*1.0+sqrt(delta))/(2*a*1.0);
-	//方程求解 
 	//printf("你要解的方程是：%dx^2+%dx+%d=0\n",a,b,c);
-	if (delta<0) printf("方程不存在实数根\n");else
-		if (delta==0) printf("方程只有一个实数根:x=%f\n",x1);else
-			if (a==0) printf("这不是一个一元二次方程\n"); else 
-				printf("方程的两个根是：x1=%f,x2=%f\n",x1,x2);
-	//根的判断 
+	printf("方程的两个根是：x1=%f,x2=%f\n",x1,x2);
+	//x=(-b+-sart(delta))/(2a)
 	
 	return 0;
 }
-//scanf("%d%d%d",&a,&b,&c);
-//x=(-b+-sart(delta))/(2a)
-	
