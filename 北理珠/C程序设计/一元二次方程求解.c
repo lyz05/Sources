86f3e541e539db5bcde0382a1fefa7b6c0251e82
lyz <liuyuanzhe0515@gmail.com>
2018-09-18 16:02:43 +0800

    update

--- 北理珠/C程序设计/一元二次方程求解.c
+++ 北理珠/C程序设计/一元二次方程求解.c
@@ -14,6 +14,15 @@ int main()
 	x2 = (-b*1.0+sqrt(delta))/(2*a*1.0);
 	//方程求解 
 	//printf("你要解的方程是：%dx^2+%dx+%d=0\n",a,b,c);
+	if (a==0 && b==0)
+		if (c==0) {
+			printf("该式为恒等式\n");
+			return 0;	
+		}
+		else {
+			printf("该式不成立\n");
+			return 0;	
+		}
 	if (delta<0) printf("方程不存在实数根\n");else
 		if (delta==0) printf("方程只有一个实数根:x=%f\n",x1);else
 			if (a==0) printf("这不是一个一元二次方程\n"); else 
