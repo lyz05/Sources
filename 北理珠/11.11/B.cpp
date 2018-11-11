dbe03fe790d9dba79769814e0735b81ed220e75e
Yuanzhe Liu <liuyuanzhe0515@gmail.com>
2018-11-11 16:45:11 +0800

    11.11update

---
+++ 北理珠/11.11/B.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main()
+{
+	freopen("B.in","r",stdin);
+	scanf("%d%d%d",&L,&R,&C);
+	for (int i=0;i<R;i ++)
+		for (int j=0;j<C;j ++)
+			scanf("%c",)
+}
--- 北理珠/11.11/F.cpp
+++ 北理珠/11.11/F.cpp
@@ -1,12 +1,57 @@
 #include <cstdio>
+#include <climits>
+#include <cstring>
+using namespace std;
 
-const int N = 5;
+const int N = 5,dir[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+int map[N+2][N+2];
+int cnt=-1,ans=INT_MAX;
+
+struct pair
+{
+	int x,y;
+} now[N*N],Ans[N*N];
+
+
+bool judge(int x,int y)
+{return x>=0 && x<N && y>=0 && y<N;}
+
+
+void dfs(int x,int y)
+{
+	if (map[x][y]==1) return;
+	map[x][y]=1;
+	now[++ cnt].x = x,now[cnt].y=y;
+	if (x==N-1 && y==N-1)
+	{
+		if (cnt<ans) 
+		{
+			ans = cnt;
+			memcpy(Ans,now,sizeof(now));
+		}
+		map[x][y] = 0;
+		cnt --;
+		return;
+	}
+	for (int i=0;i<4;i ++)
+	{
+		int tx = x+dir[i][0],ty = y+dir[i][1];
+		if (judge(tx,ty))
+			dfs(tx,ty);
+	}
+	map[x][y] = 0;
+	cnt --;
+}
 
 int main()
 {
-	for (int i=1;i<=N;i ++)
-		for (int j=1;j<=N;j ++)
+	freopen("F.in","r",stdin);
+	for (int i=0;i<N;i ++)
+		for (int j=0;j<N;j ++)
 			scanf("%d",&map[i][j]);
-	
+	dfs(0,0);
+	//printf("%d\n",ans);
+	if (ans<=N*N)
+		for (int i=0;i<=ans;i ++) printf("(%d,%d)\n",Ans[i].x,Ans[i].y);
 	return 0;
 }
