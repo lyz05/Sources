f870c81f2a307f7eaad6aaef5189dbc68478773c
Yuanzhe Liu <liuyuanzhe0515@gmail.com>
2018-12-21 11:29:53 +0800

    12.21
    12.21

--- 北理珠/C程序设计小项目/多文件/Program.c
+++ 北理珠/C程序设计小项目/多文件/Program.c
@@ -1,78 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<conio.h>			//密码输入 
-#include<io.h>				//文件存在检测 
+#include "Program.h"
 
-typedef struct c{
-	int id;
-	char name[32];
-}Cinfo;
-//学院信息 
-typedef struct gd{
-	int id;
-	char name[16];
-}Sinfo;
-//性别信息 
-typedef struct stu{
-	int id;
-	char name[16];
-	int sid;				//性别代码
-	int cid;				//学院代码
-	int score[10];			//10门成绩
-	float ave;				//平均成绩 
-	//int nm;				//成绩名次 
-}STU;
-//学生信息 
-
-typedef int (*CompareMethod)(STU x,STU y);
-//CompareMethod 比较方式函数指针 
-
-char filenameStu[]="data1\\Stu_Info3.txt",filenameC[]="data1\\C_Info1.txt",filenameS[]="data1\\S_Info2.txt",filenamePsd[]="data1\\psd.txt";
+char filenameStu[]="data\\Stu_Info3.txt",filenameC[]="data\\C_Info1.txt",filenameS[]="data\\S_Info2.txt",filenamePsd[]="data\\psd.txt";
 Cinfo c[10];
 Sinfo s[3];
 STU stu[100],re[100];						//学生信息，返回的学生信息
 int para[100];
 int num = 0,cnum = 0,snum = 0;				//学生总数,学院总数，性别总数 
-
-void menu1();
-void menu2();
-void menu3();
-void menu4();
-void menu5();
-void menu6();
-void menu7();
-void menu8();
-void menu0();
-int error();								//未读入文件检测 
-void init();								//系统初始化&密码检测 
-void printStu(STU stu);
-void seekPos(FILE *fp);
-//过滤文件中最顶部的注释 
-int inputStu(STU stu[], int *num, char filename[]);    
-//从文件名filename读num个学生信息到数组stu，并同时计算平均成绩,成功返回真，失败返回假 
-int inputCinfo(Cinfo x[], int *num, char filename[]);   
-//从文件名filename读num个学院信息到数组x,成功返回真，失败返回假
-int inputSinfo(Sinfo x[], int *num, char filename[]);
-//从文件名filename读num个性别信息到数组x,成功返回真，失败返回假
-void sortStudents(STU *s, int *para, int num,CompareMethod cm);
-//对num个学生按照指定比较方式排序
-int sortByAve(STU x, STU y);
-//根据平均成绩排序
-int sortByName(STU x, STU y);
-//根据姓名排序
-int findName(STU x[], int num, char name[], STU re[]); 
-//根据给定的name，从num个x中找出同名，并依次存放到re中，函数返回找到的人数
-int ModifyStuinfo(STU x[], int num, int id, char filename[]);
-//根据给定的学生学号修改该学生的信息
-int JudgePsd(char psd[]);
-//判断密码正确性
-int ModifyPsd(char psd[],char filename[]);
-//修改密码，返回修改是否成功
-char* encodeInp(char *input);
-//对密码进行加密 
-void InputPsd(char psd[]);
-//密码输入函数 
 
 int main()
 {
@@ -120,23 +53,6 @@ void menu0()
 	exit(0);
 }
 
-void menu1()
-{
-	int tmp;
-	if (inputStu(stu,&tmp,filenameStu))
-		printf("读入学生信息成功，共读入%d个信息\n",tmp),num = tmp;
-	else
-		printf("读入学生信息失败\n");
-	if (inputCinfo(c,&tmp,filenameC))
-		printf("读入学院信息成功，共读入%d个信息\n",tmp),cnum = tmp;
-	else
-		printf("读入学院信息失败\n"); 
-	if (inputSinfo(s,&tmp,filenameS)) 
-		printf("读入性别信息成功，共读入%d个信息\n",tmp),snum = tmp;
-	else
-		printf("读入性别信息失败\n");
-}
-
 void menu2()
 {
 	if (error()) return;
@@ -145,13 +61,7 @@ void menu2()
 	for (i=0;i<num;i ++) printStu(stu[para[i]]);
 }
 
-void menu3()
-{
-	if (error()) return;
-	sortStudents(stu,para,num,sortByAve);
-	int i;
-	for (i=0;i<num;i ++) printStu(stu[para[i]]);
-}
+
 
 void menu4()
 {
@@ -318,90 +228,11 @@ void menu8()
 	puts("***************************************************************"); 
 } 
 
-int inputStu(STU stu[], int *num, char filename[])
-{
-	FILE *fp;
-	if ((fp = fopen(filename,"r"))==NULL) return 0;
-	seekPos(fp);
-		
-	int i,j;
-	//跳到读写位置 
-	fscanf(fp,"%d",num);
-	for (i=0;i<(*num);i ++)
-	{
-		fscanf(fp,"%d %s %d %d",&stu[i].id,stu[i].name,&stu[i].sid,&stu[i].cid);
-		if (ferror(fp)) return 0;
-		//printf("%d %s %d %d",stu[i].id,stu[i].name,stu[i].sid,stu[i].cid); 
-		stu[i].ave = 0;
-		for (j=0;j<10;j ++)
-		{
-			fscanf(fp,"%d",&stu[i].score[j]);
-			if (ferror(fp)) return 0;
-			stu[i].ave += stu[i].score[j];
-			//printf("%d ",stu[i].score[j]);
-		}
-		stu[i].ave /= 10;
-		//printf("%f\n",stu[i].ave);
-	}
-	
-	
-	if (fclose(fp)==EOF) return 0;
-	return 1;
-}
 
-int inputCinfo(Cinfo x[], int *num, char filename[])
-{
-	FILE *fp;
-	if ((fp = fopen(filename,"r"))==NULL) return 0;
-	seekPos(fp);
-	
-	int i; 
-	fscanf(fp,"%d",num);
-	for (i=0;i<(*num);i ++)
-	{
-		fscanf(fp,"%d %s",&x[i].id,x[i].name);
-		//printf("%d %s\n",x[i].id,x[i].name);
-		if (ferror(fp)) return 0;
-	}
-	
-	if (fclose(fp)==EOF) return 0;
-	return 1;
-} 
 
-int inputSinfo(Sinfo x[], int *num, char filename[])
-{
-	FILE *fp;
-	if ((fp = fopen(filename,"r"))==NULL) return 0;
-	seekPos(fp);
-	
-	int i; 
-	fscanf(fp,"%d",num);
-	for (i=0;i<(*num);i ++)
-	{
-		fscanf(fp,"%d %s",&x[i].id,x[i].name);
-		//printf("%d %s\n",x[i].id,x[i].name);
-		if (ferror(fp)) return 0;
-	}
-	
-	if (fclose(fp)==EOF) return 0;
-	return 1;	
-} 
 
-int sortByAve(STU x, STU y) {return x.ave < y.ave;}
 int sortByName(STU x, STU y) {return strcmp(x.name,y.name)>0;}
-void sortStudents(STU *s, int *para, int num, CompareMethod cmp)
-{
-	int i,j,tmp;
-	for(i=0;i<num;i++) para[i]=i;
-	//平行数组初始化 
-	for(i=0;i<num;i++)
-	    for(j=i+1;j<num;j++)
-	    	if(cmp(s[para[i]],s[para[j]])){  
-				tmp = para[i];
-				para[i] = para[j];
-				para[j] = tmp;	
-			}
-}
+
 
 int findName(STU x[], int num, char name[], STU re[])
 {
