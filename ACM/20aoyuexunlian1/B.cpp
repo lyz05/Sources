b88f2c9d9b75d2950a862cf234c10c922767af8e
lyz05 <294068487@qq.com>
2020-04-09 14:30:07 +0800

    4.9

---
+++ ACM/20aoyuexunlian1/B.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	freopen("B.in","r",stdin);
+	//freopen(".out","w",stdout);
+    cin >> T;
+    while (T --)
+    {
+        
+    }
+	return 0;
+}
