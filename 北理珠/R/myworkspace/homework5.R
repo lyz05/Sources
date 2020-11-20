cfb9b7a909bf4a2c7ab4523d2822bb84017044c3
Yuanzhe Liu <liuyuanzhe0515@gmail.com>
2020-11-20 14:23:16 +0800

    Update

--- 北理珠/R/myworkspace/homework5.R
+++ 北理珠/R/myworkspace/homework5.R
@@ -12,4 +12,7 @@ vdata <- data[-train_sub,]##
 library(rpart)
 dtree<-rpart(V2~.,data=tdata,method="class", parms=list(split="information"))
 printcp(dtree)
-
+tree<-prune(dtree,cp=0.0125)
+rpart.plot(dtree,branch=1,type=2, fallen.leaves=T,cex=0.8, sub="剪枝前")
+rpart.plot(tree,branch=1, type=4,fallen.leaves=T,cex=0.8, sub="剪枝后")
+a
