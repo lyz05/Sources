98b6cf09918f8fb6e75f11cf0d81305dc26d5ab7
Yuanzhe Liu <liuyuanzhe0515@gmail.com>
2020-12-22 22:10:35 +0800

    Update

---
+++ ACM/20新生杯/找朋友.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<int,int> a;
+int T,n;
+
+int main()
+{
+	freopen("1.in","r",stdin);
+	freopen("0.out","w",stdout);
+	cin >> T;
+	while (T --) {
+		a.clear();
+		int ans = 0;
+		cin >> n;
+		for (int i=1;i<=n;i ++){
+			int t;
+			cin >> t;
+			a[t] ++;
+		}
+		for(map<int,int>::iterator it=a.begin();it != a.end();it ++)
+        {
+        	int t = it -> second;
+        	ans += t*(t-1)/2;
+        }
+        cout << ans << endl;
+	}
+	return 0;
+ } 
